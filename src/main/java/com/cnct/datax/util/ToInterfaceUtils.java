27b7aed9f14f019b60f834955107005a18a0e2b5
liyang <493123519@qq.com>
2020-04-22 08:55:13 +0800

    实现远程调用解密md5方法

---
+++ src/main/java/com/cnct/datax/util/ToInterfaceUtils.java
@@ -0,0 +1,104 @@
+package com.cnct.datax.util;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+
+public class ToInterfaceUtils {
+
+    /**
+     * 调用对方接口方法
+     * @param path 对方或第三方提供的路径
+     * @param data 向对方或第三方发送的数据，大多数情况下给对方发送JSON数据让对方解析
+     */
+    public static String interfaceUtil(String path, String data) {
+        try {
+            URL url = new URL(path);
+            //打开和url之间的连接
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            PrintWriter out = null;
+            //请求方式
+            conn.setRequestMethod("POST");
+//           //设置通用的请求属性
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
+            //设置是否向httpUrlConnection输出，设置是否从httpUrlConnection读入，此外发送post请求必须设置这两个
+            //最常用的Http请求无非是get和post，get请求可以获取静态页面，也可以把参数放在URL字串后面，传递给servlet，
+            //post与get的 不同之处在于post的参数不是放在URL字串里面，而是放在http请求的正文内。
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            //获取URLConnection对象对应的输出流
+            out = new PrintWriter(conn.getOutputStream());
+            //发送请求参数即数据
+            out.print(data);
+            //缓冲数据
+            out.flush();
+            //获取URLConnection对象对应的输入流
+            InputStream is = conn.getInputStream();
+            //构造一个字符流缓存
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String str = "";
+            while ((str = br.readLine()) != null) {
+                System.out.println(str);
+            }
+
+            //关闭流
+            is.close();
+            //断开连接，最好写上，disconnect是在底层tcp socket链接空闲时才切断。如果正在被其他线程使用就不切断。
+            //固定多线程的话，如果不disconnect，链接会增多，直到收发不出信息。写上disconnect后正常一些。
+            conn.disconnect();
+            System.out.println("完整结束");
+            return str;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String httpUrlConnection(String pathUrl, String params){
+        try {
+            // 建立连接
+            URL url = new URL(pathUrl);
+            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+            //设置连接属性
+            httpConn.setDoOutput(true);// 使用 URL 连接进行输出
+            httpConn.setDoInput(true);// 使用 URL 连接进行输入
+            httpConn.setUseCaches(false);// 忽略缓存
+            httpConn.setRequestMethod("POST");// 设置URL请求方法
+            String requestString = params;
+            // 设置请求属性
+            // 获得数据字节数据，请求数据流的编码，必须和下面服务器端处理请求流的编码一致
+            byte[] requestStringBytes = requestString.getBytes("UTF-8");
+            httpConn.setRequestProperty("Content-length", "" + requestStringBytes.length);
+            httpConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            httpConn.setRequestProperty("Connection", "Keep-Alive");// 维持长连接
+            httpConn.setRequestProperty("Charset", "UTF-8");
+            // 建立输出流，并写入数据
+            OutputStream outputStream = httpConn.getOutputStream();
+            outputStream.write(requestStringBytes);
+            outputStream.close();
+            // 获得响应状态
+            int responseCode = httpConn.getResponseCode();
+            if(HttpURLConnection.HTTP_OK == responseCode){
+                // 当正确响应时处理数据
+                StringBuffer sb = new StringBuffer();
+                String readLine;
+                BufferedReader responseReader;
+                // 处理响应流，必须与服务器响应流输出的编码一致
+                responseReader = new BufferedReader(new InputStreamReader(httpConn.getInputStream(), "UTF-8"));
+                while ((readLine = responseReader.readLine()) != null){
+                    sb.append(readLine).append("\n");
+                }
+                responseReader.close();
+                return sb.toString();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
